938977b68275beae0eb07e33004d71ec14d71c8e
icksonb <ickson94@gmail.com>
2020-08-27 17:26:38 -0300

    Novas mensagens da comunicação Bluetooth.

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -5,7 +5,8 @@
  
 #include <iostream>
 #include <string>
- 
+#include <string>
+
 BLECharacteristic *pCharacteristic;
  
 bool deviceConnected = false;
@@ -35,6 +36,21 @@ class MyServerCallbacks: public BLEServerCallbacks
  
 class MyCallbacks: public BLECharacteristicCallbacks 
 {
+
+   int indexof(std::string& text, std::string& pattern)
+   {
+      // where appears the pattern in the text?
+      std::string::size_type loc = text.find(pattern, 0);
+      if(loc != std::string::npos)
+      {
+          return loc;
+      }
+      else
+      {
+          return -1;
+      }
+   }
+
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string rxValue = pCharacteristic->getValue();
       Serial.println(rxValue[0]);
@@ -49,18 +65,41 @@ class MyCallbacks: public BLECharacteristicCallbacks
         Serial.println();
         Serial.println("*********");
 
+        std::string SSID = "{dotA:D:S";
+        std::string pass = "{dotA:D:P";
+        
         if(rxValue.compare("{dotA:G:I}") == 0)
           pCharacteristic->setValue("{0001}");
         else if(rxValue.compare("{dotA:G:L}") == 0)
           pCharacteristic->setValue("{0003}");
-        else if(rxValue.compare("{dotA:D:S:sal}") == 0)
-          pCharacteristic->setValue("{dotA:D:S:sal}");
-        else if(rxValue.compare("{dotA:D:P:teste}") == 0)
-          pCharacteristic->setValue("{dotA:D:P:teste}");
+        else if(indexof(rxValue, SSID) != -1)
+          pCharacteristic->setValue(rxValue);
+        else if(indexof(rxValue, pass) != -1)
+          pCharacteristic->setValue(rxValue);
         else if(rxValue.compare("{dotA:G:S}") == 0)
           pCharacteristic->setValue("{sal}");
         else if(rxValue.compare("{dotA:G:P}") == 0)
           pCharacteristic->setValue("{teste}");
+        else if(rxValue.compare("{dotA:V:T:0}") == 0)
+          pCharacteristic->setValue("{50.1}");
+        else if(rxValue.compare("{dotA:V:T:1}") == 0)
+          pCharacteristic->setValue("{0.12}");
+        else if(rxValue.compare("{dotA:V:T:2}") == 0)
+          pCharacteristic->setValue("{10.3}");
+        else if(rxValue.compare("{dotA:V:T:3}") == 0)
+          pCharacteristic->setValue("{59.39}");
+        else if(rxValue.compare("{dotA:V:V:0}") == 0)
+          pCharacteristic->setValue("{140.23}");
+        else if(rxValue.compare("{dotA:V:D:0}") == 0)
+          pCharacteristic->setValue("{1}");
+        else if(rxValue.compare("{dotA:V:D:1}") == 0)
+          pCharacteristic->setValue("{0}");
+        else if(rxValue.compare("{dotA:V:D:2}") == 0)
+          pCharacteristic->setValue("{0}");
+        else if(rxValue.compare("{dotA:V:D:3}") == 0)
+          pCharacteristic->setValue("{1}");
+        else if(rxValue.compare("{dotA:V:P:0}") == 0)
+          pCharacteristic->setValue("{1}");
         else
           pCharacteristic->setValue("{0}");
 
